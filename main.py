dd0d1832ee28b73490cf675bfe34878759d11268
abelgarcia4545 <149451272+abelgarcia4545@users.noreply.github.com>
2023-11-06 19:58:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-mnum = int(input('Enter the number of male students: '))
-fnum = int(input('Enter the number of female students: '))
+    mnum = int(input('Enter the number of male students: '))
+    fnum = int(input('Enter the number of female students: '))
 
 
-total = mnum + fnum
-m_perc = mnum / total * 100
-f_perc = fnum / total * 100
+    total = mnum + fnum
+    m_perc = mnum / total * 100
+    f_perc = fnum / total * 100
     """
     ########################################
     # Do not delete the return statement
